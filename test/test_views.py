fc5e5e76d08c7fe53424366c33185a613f7baed7
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-11-12 22:00:32 +0200

    Hanna

--- test/test_views.py
+++ test/test_views.py
@@ -15,4 +15,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/output')
-        self.assertEqual(b'{ "imie":"Wadzim", "msg":"Hello World!"}', rv.data)
+        self.assertEqual(b'{ "imie":"Wadzim", "mgs":"Hello World!"}', rv.data)
